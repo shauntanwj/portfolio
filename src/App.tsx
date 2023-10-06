16813db50e3997bac3a191d96695fb737d4d418b
shauntanwj <shauntanwj10@gmail.com>
2023-10-06 13:38:52 +0800

    Initial Commit with Chakra UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ButtonList from "./components/ButtonList";
+
+function App() {
+  return (
+    <>
+      <ButtonList></ButtonList>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ButtonList.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@chakra-ui/react";
+
+const ButtonList = () => {
+  return <Button colorScheme="blue">Button</Button>;
+};
+
+export default ButtonList;
