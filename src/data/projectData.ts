3b1bc3e091360c50275fb8a2ce4c8598186a3dfc
shauntanwj <shauntanwj10@gmail.com>
2023-10-30 15:44:44 +0800

    update: updated project data category

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -11,6 +11,15 @@ export interface Project {
 export const ProjectData = [
   {
     id: 1,
+    project_name: "Client Information Web Scrapper",
+    asset_name: "scraper",
+    asset_type: "img",
+    year: 2023,
+    category: "Personal",
+    category_detail: "Web Scrapping",
+  },
+  {
+    id: 2,
     project_name:
       "Profiling Malaysian’s Politician’s Political Influence Based on the Sentiment of the Public",
     asset_name: "whosYourYb",
@@ -20,7 +29,7 @@ export const ProjectData = [
     category_detail: "User Generated Content",
   },
   {
-    id: 2,
+    id: 3,
     project_name: "Real Time Fire and Climate Data Streaming Simulation",
     asset_name: "fire",
     asset_type: "vid",
@@ -29,7 +38,7 @@ export const ProjectData = [
     category_detail: "Big Data Management",
   },
   {
-    id: 3,
+    id: 4,
     project_name:
       "Tsunami Detection in a Distributed Wireless Sensor Network (WSN)",
     asset_name: "tsunamiDetection",
@@ -39,21 +48,12 @@ export const ProjectData = [
     category_detail: "Parallel Computing",
   },
   {
-    id: 4,
+    id: 5,
     project_name: "Dinosaur Park",
     asset_name: "dinosaur",
     asset_type: "vid",
     year: 2021,
     category: "Academic",
-    category_detail: "Java Console Game",
-  },
-  {
-    id: 5,
-    project_name: "Client Information Web Scrapper",
-    asset_name: "scraper",
-    asset_type: "img",
-    year: 2023,
-    category: "Personal",
-    category_detail: "Web Scrapping",
+    category_detail: "Object Oriented Programming (OOP)",
   },
 ];
